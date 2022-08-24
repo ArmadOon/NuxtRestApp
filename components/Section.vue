3cbbca188f5d12365bb2641f61b969ef1b3c5603
Pluhy <pluhar.martin@outlook.com>
2022-08-24 14:31:12 +0200

    Znova

---
+++ components/Section.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Restaurantly</h1>
+    <a href="/restaurants">Go to restaurants</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Section"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nav />
+    <Section />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
+<script>
+import Section from "../components/Section";
+export default {
+  components: {Section}
+}
+</script>
